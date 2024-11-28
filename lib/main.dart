59f6fcf0cc0363af5d90e990eba1e46f213a1b02
Sumit Mishra <116886410+sumitmishra-29@users.noreply.github.com>
2024-11-29 00:35:05 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WorkStatus App',
+      title: 'Vinove_attandance',
       theme: ThemeData(primarySwatch: Colors.purple),
       home: DashboardScreen(),
     );
